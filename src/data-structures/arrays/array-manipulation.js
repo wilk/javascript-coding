78c50f385786fbc03f45887485d5b7151b5d74ed
Wilk <wilk3ert@gmail.com>
2017-10-26 08:05:04 +0200

    Add array-manipulation algorithm

--- src/data-structures/arrays/array-manipulation.js
+++ src/data-structures/arrays/array-manipulation.js
@@ -1,26 +1,35 @@
-const n = 5,
+const n = 9,
   list = Array.from('0'.repeat(n)).map(Number),
-  operations = [
+  /*operations = [
     {a: 1, b: 2, k: 100},
     {a: 2, b: 5, k: 100},
     {a: 3, b: 4, k: 100},
     {a: 1, b: 2, k: 300},
     {a: 5, b: 5, k: 700}
-  ]
+  ],
+  operations = [
+    {a: 3, b: 5, k: 100},
+    {a: 1, b: 2, k: 100},
+    {a: 2, b: 3, k: 100},
+    {a: 5, b: 5, k: 100},
+    {a: 1, b: 1, k: 100},
+    {a: 4, b: 5, k: 100},
+    {a: 3, b: 4, k: 100},
+    {a: 2, b: 4, k: 100}
+  ]*/
+operations = [{a:2,b:3,k:100},{a:6,b:7,k:100}]
 
 let max = 0
 operations.forEach(({a, b, k}) => {
-  if (k === 0) return
-
-  a--
-  b--
-
-  let index = a
-  do {
-    list[index] += k
-    if (list[index] > max) max = list[index]
-    index++
-  } while (index <= b)
+  list[a-1] += k
+  if (b < n) list[b] -= k
+  console.log(list)
 })
+
+let sum = max
+for (let el of list) {
+  sum += el
+  if (max < sum) max = sum
+}
 
 console.log(max)
